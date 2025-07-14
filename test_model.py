770f4e1c4f83138b419686a77d79845081c34b9e
chtrn302 <traconghau0212@gmail.com>
2025-07-14 08:21:09 +0700

    In ra result chi tiết

--- test_model.py
+++ test_model.py
@@ -5,17 +5,17 @@ from PIL import Image
 
 eye_model = models.load_model(os.path.join('models','model.keras'))
 
-img_path = os.path.join('data','') # Import image to test
+img_path = os.path.join('data','path') # Import image to test
 img = Image.open(img_path)
 img = img.resize((80,80))
 img_array = np.array(img) / 255.0
 img_array = np.expand_dims(img_array, axis=0)
 
-result = eye_model.predict(img_array)
+result = eye_model.predict(img_array, verbose=0)
 
-if result[0] >= 0.8:
+if result[0][0] >= 0.5:
     print('Mo')
-    print(result)
+    print(result[0][0])
 else:
     print('Dong')
-    print(result)
+    print(result[0][0])
