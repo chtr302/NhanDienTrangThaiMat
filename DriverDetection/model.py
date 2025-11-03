f00b24990077502c99b6dc4ed12028e36199ddb8
chtrn302 <traconghau0212@gmail.com>
2025-11-03 18:14:03 +0700

    Sử dụng model mới xây dựng mạnh mẽ hơn. Thêm class để sử dụng Layer tùy chỉnh

--- DriverDetection/model.py
+++ DriverDetection/model.py
@@ -1,6 +1,34 @@
 import os
 from tensorflow.keras import models as keras_models
+from tensorflow.keras import layers
+import tensorflow as tf
 
+class SpatialAttention(layers.Layer):
+    def __init__(self, kernel_size=7, **kwargs):
+        super(SpatialAttention, self).__init__(**kwargs)
+        self.kernel_size = kernel_size
+        self.conv2d = layers.Conv2D(
+            filters=1,
+            kernel_size=self.kernel_size,
+            strides=1,
+            padding='same',
+            activation='sigmoid',
+            kernel_initializer='he_normal',
+            use_bias=False,
+            name='spatial_attention_conv'
+        )
+
+    def call(self, inputs):
+        avg_pool = tf.reduce_mean(inputs, axis=3, keepdims=True)
+        max_pool = tf.reduce_max(inputs, axis=3, keepdims=True)
+        concat = layers.Concatenate(axis=3)([avg_pool, max_pool])
+        attention = self.conv2d(concat)
+        return layers.Multiply()([inputs, attention])
+
+    def get_config(self):
+        config = super(SpatialAttention, self).get_config()
+        config.update({'kernel_size': self.kernel_size})
+        return config
 
 class Models:
     """Loader for pretrained CNN models used at runtime."""
@@ -12,18 +40,18 @@ class Models:
 
     def _choose_eye_model_path(self) -> str:
         candidates = [
-            os.path.join(self.models_dir, 'model.keras'),
-            os.path.join(self.models_dir, 'best_model_first_try.keras'),
+            os.path.join(self.models_dir, 'model_1st.keras'),
         ]
         for p in candidates:
             if os.path.exists(p):
+                print(f"Loading eye model from: {p}")
                 return p
         raise FileNotFoundError(f"No eye model found in {self.models_dir}")
 
     def load_eye_model(self):
         path = self._choose_eye_model_path()
-        print(f"Loading eye model from: {path}")
-        model = keras_models.load_model(path)
-        print("Eye model loaded OK")
+        model = keras_models.load_model(
+            path,
+            custom_objects={'SpatialAttention': SpatialAttention}
+        )
         return model
-
